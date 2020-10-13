9005d89f5a5279846f9e2718560e3907776dfa1f
Arica Jackson <aricajackson2020@gmail.com>
2020-10-12 22:44:54 -0400

    Additional query

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -10,3 +10,25 @@ FROM employees as es
 	JOIN salaries as sal
 	ON (sal.emp_no = es.emp_no)
 ORDER BY (es.last_name, es.first_name);
+
+-- List first name, last name, and hire date for employees who were hired in 1986
+
+-- SELECT 	es.first_name,
+-- 		es.last_name,
+-- 		es.hire_date 
+-- FROM employees as es
+-- WHERE 	es.hire_date = 1986
+-- ORDER BY (es .last_name, es.first_name);	
+
+-- List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, 
+-- last name, first name. 
+SELECT 	de.dept_no,
+		de.dept_name,
+		demg.emp_no,
+		es.last_name,
+		es.first_name
+FROM 	departments as de, dept_manager as demg,employees as es
+ORDER BY (es.last_name, es.first_name); 
+		
+
